305136b1a201e86eeaf4d3308615ee0eceb93525
kr-kirilova <k.kirilova2019@pmg-pernik.com>
2023-02-07 13:25:38 +0200

    add file

---
+++ statisticArray/statisticArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace statisticArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Console.WriteLine(OptionsManager.CountLess100(arr));
+            Console.WriteLine(OptionsManager.MaxNum(arr));
+            OptionsManager.EvenNum(arr);
+            Console.WriteLine(OptionsManager.SumEven(arr));
+            Console.WriteLine(OptionsManager.CountEven(arr));
+            OptionsManager.ReverseArr(arr);
+
+
+        }
+    }
+}
