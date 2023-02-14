409ed57a3f081ff0b98082fdfc5617b6acb1c49c
kr-kirilova <k.kirilova2019@pmg-pernik.com>
2023-02-14 12:59:17 +0200

    add removeNum

---
+++ ListTasks/ListTasks/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            var last = nums[nums.Count - 1];
+             nums.RemoveAll(x=>x==last);
+            Console.WriteLine(string.Join(" " , nums));
+        }
+    }
+}
